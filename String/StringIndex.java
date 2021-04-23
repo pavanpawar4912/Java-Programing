b0b9c3c715e1e37aae2e038a6588dba72cf78f34
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-04-23 12:21:40 +0530

    Create StringIndex.java

---
+++ String/StringIndex.java
@@ -0,0 +1,14 @@
+class StringIndex
+{
+public static void main(String args[])
+{
+int index;
+String a = "Welcome To Java";
+index = a.indexOf('w');
+System.out.println("index :"+index);
+index = a.indexOf('e');
+System.out.println("index:"+index);
+index = a.indexOf('J');
+System.out.println("index: " +index);
+}
+}
