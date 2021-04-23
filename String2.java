c611bb5f2196d4a73ebc7045a24389b9b5027959
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-04-23 10:58:26 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,12 @@
+class String2
+{
+public static void main(String args[])
+{
+String a = "Welcome";
+String b = "To";
+String c = "Java";
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
