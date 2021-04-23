18f68a109c4022dd13b5ac03c9a2b49444196e36
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-04-23 10:50:57 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,20 @@
+class String1
+{
+public static void main(String args[])
+{
+int i;
+String a = "welcome";
+String b = "To";
+String c = "Java";
+System.out.println(a);
+i = a.length();
+System.out.println("length = "+i);
+System.out.println(b);
+i=b.length();
+System.out.println("length = "+i);
+System.out.println(c);
+i = c.length();
+System.out.println("length ="+i);
+
+}
+}
