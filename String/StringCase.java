23ee9ab7deebb6c1042c3cc5f51f78b46ebbb0ca
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-04-24 11:23:28 +0530

    Add files via upload

---
+++ String/StringCase.java
@@ -0,0 +1,12 @@
+class StringCase
+{
+public static void main(String args[])
+{
+String a = "Welcome";
+String b = "To";
+String c = "Java";
+System.out.println(a.toLowerCase());
+System.out.println(b.toUpperCase());
+System.out.println(c.toLowerCase());
+}
+}
