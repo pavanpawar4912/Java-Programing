ed2f230b126276cf488c7368acbcae631d1c946b
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-01 11:16:20 +0530

    Create number1.java

---
+++ Pattern printing/number1.java
@@ -0,0 +1,17 @@
+class number1
+{
+	public static void main(String [] args)
+	{
+		int i,j,no,n= 7;
+		for(i=0; i<n; i++)
+		{
+			no = 1;
+			for (j=0; j<=i; j++ )
+			{
+				System.out.print(no+" ");
+				no++;
+			}
+			System.out.println();
+		}
+	}
+}
