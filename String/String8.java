4a76f1bd195135a3be654038f4302674afea443c
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-01 09:55:55 +0530

    Add files via upload

---
+++ String/String8.java
@@ -0,0 +1,12 @@
+class String8
+{
+public static void main(String args[])
+{
+  String a = "Welcome";
+  String b= "Java";
+  System.out.println(a);
+  System.out.println(b);
+  boolean c = a.equals(b);
+  System.out.println(c);
+}
+}
