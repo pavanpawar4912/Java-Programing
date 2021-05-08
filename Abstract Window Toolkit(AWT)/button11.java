fc28d90ac4ec5c29eb47bc70efedd13af2492073
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-08 18:22:05 +0530

    Create button11.java

---
+++ Abstract Window Toolkit(AWT)/button11.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+class button11 extends Frame{
+button11()
+{
+Button b=new Button();
+b.setBounds(40,100,80,30);
+add(b);
+setSize(400,400);
+setLayout(null);
+setVisible(true);
+}
+public static void main(String args[])
+{
+button11 bb = new button11();
+}
+}
